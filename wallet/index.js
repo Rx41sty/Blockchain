6d3a7b1242b2fbcd04bf3d48f0378660b607ee08
George <gbaramidze1247@gmail.com>
2020-11-05 14:22:07 +0400

    added elliptic and generated publicKey

--- wallet/index.js
+++ wallet/index.js
@@ -1,10 +1,15 @@
 const { STARTING_BALANCE } = require("../config");
+const { ec } = require("../util");
 
 class Wallet
 {
     constructor()
     {
         this.balance = STARTING_BALANCE;
+
+        const keypair = ec.genKeyPair();
+
+        this.publicKey = keypair.getPublic().encode("hex");
     }
 }
 
