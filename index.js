7144481e6f388ac5390f79117f19f2fea94911fb
George <gbaramidze1247@gmail.com>
2021-02-03 20:10:36 +0400

    Started frontend

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const request = require('request');
+const path = require('path');
 const Blockchain = require("./blockchain");
 const TransactionPool = require("./wallet/transaction-pool");
 const PubSub = require("./app/pubsub");
@@ -73,6 +74,9 @@ app.get("/api/wallet-info", (req, res) => {
 	res.json({address, balance: Wallet.calculateBalance({ chain:blockchain.chain, address })})
 });
 
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, './client/index.html'));
+});
 
 
 
