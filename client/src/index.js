c29df3156953bcd734c690989596a21c241e54ee
George <gbaramidze1247@gmail.com>
2021-02-04 21:08:49 +0400

    Added components

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
+import App from './components/App'
 
 
-
-render(<div>Third From React</div>, document.getElementById('root'));
+render(<App />, document.getElementById('root'));
