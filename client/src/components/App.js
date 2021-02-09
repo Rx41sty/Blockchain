ae54b2538fa23566e33b8f1d49eee480f68c1446
George <gbaramidze1247@gmail.com>
2021-02-09 21:29:12 +0400

    Added request to /api/wallet-info/ from react

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,9 +2,27 @@ import React, { Component } from 'react';
 
 class App extends Component
 {
+	state = {walletInfo: {}};
+
+	componentDidMount()
+	{
+		fetch("http://localhost:3000/api/wallet-info")
+		.then(response => response.json())
+		.then(json => this.setState({ walletInfo : json }));
+		//.then(json => console.log(json)));
+	}
+
+
 	render()
 	{
-		return (<div>Welcome to the Blockchain</div>);
+		const {address, balance} = this.state.walletInfo;
+		return (
+			<div>
+			Welcome to the Blockchain
+				<div>Address: {address}</div>
+				<div>Balance: {balance}</div>
+			</div>
+			);
 	}
 	
 }
