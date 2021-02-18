35a948a45d66dbd1a01d2e0b4b008266d11e111a
George <gbaramidze1247@gmail.com>
2021-02-18 13:26:52 +0400

    Start

---
+++ client/src/components/TransactionPool.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+import Transaction from './Transaction';
+import { Link } from 'react-router-dom';
+import history from '../history';
+
+const POLL_INERVAL_MS = 10000;
+
+class TransactionPool extends Component {
+  state = { transactionPoolMap: {} };
+
+
+
+  render() {
+    return (
+      <div className='TransactionPool'>
+        <div><Link to='/'>Home</Link></div>
+        <h3>Transaction Pool</h3>
+        {
+          Object.values(this.state.transactionPoolMap).map(transaction => {
+            return (
+              <div key={transaction.id}>
+                <hr />
+                <Transaction transaction={transaction} />
+              </div>
+            )
+          })
+        }
+        <hr />
+
+      </div>
+    )
+  }
+}
+
+export default TransactionPool;
